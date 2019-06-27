6aa1c92b1d023c4436e738fc7cc834923bafed05
laury bretteville <laury.bretteville@edhec.com>
2019-06-27 10:51:53 +0200

    refreshing the css

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,6 +33,8 @@ class OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    @order.date_order = DateTime.now
+    @order.code_validated_time = DateTime.now
 
     if @order.update(order_params)
       redirect_to shops_path
